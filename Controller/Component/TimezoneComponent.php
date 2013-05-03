581f71c2df3321d4e1a378dcb2fbf5378c1667ef
Jamie from Desktop <jamiebclark@gmail.com>
2013-05-03 12:35:21 -0400

    Fixed timezone component

--- Controller/Component/TimezoneComponent.php
+++ Controller/Component/TimezoneComponent.php
@@ -7,35 +7,30 @@ class TimezoneComponent extends Component {
 	
 	var $controller;
 	
-	var $User;
-	
 	function startup(Controller $controller) {
-		if (!empty($controller->User)) {
-			$this->User = $controller->User;
-		} else {
-			$this->User = ClassRegistry::init('User');
-		}
 		$this->controller = $controller;
-		
 		$this->set();
 		return true;
 	}
 	
-	function set($reset = false) {
-		if (!empty($this->User) && $this->Session->check('Auth.User.timezone_id')) {
-			$tzResult = $this->User->Timezone->read(null,$this->Session->read('Auth.User.timezone_id'));
-			if ($reset || !empty($tzResult)) {
-				$timezone = $tzResult['Timezone']['title'];
-				$timezoneOffset = $tzResult['Timezone']['offset'];
-
-				setMysqlConnect();
-
-				date_default_timezone_set($timezone);
-				mysql_query('SET time_zone = '.$timezoneOffset.'');
-			
-				$this->controller->set(compact('timezone'));
-				$this->controller->set(compact('timezoneOffset'));
-			}
+	function setId($timezoneId) {
+		$Timezone = ClassRegistry::init('Location.Timezone');
+		$result = $Timezone->findById($timezoneId);
+		if (!empty($result)) {
+			return $this->set($result['Timezone']['title'],$result['Timezone']['offset']);
 		}
+		return false;
 	}
+	
+	function set($timezone, $timezoneOffset) {
+		$Timezone = ClassRegistry::init('Location.Timezone');
+
+		date_default_timezone_set($timezone);
+		putenv("TZ-$timezone");
+		
+		$Timezone->query('SET time_zone = '.$timezoneOffset.'');
+	
+		$this->controller->set(compact('timezone'));
+		$this->controller->set(compact('timezoneOffset'));
+	}	
 }
