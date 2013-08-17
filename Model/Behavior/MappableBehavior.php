b26c180708ae1f8dcdffd520092cb416614ff082
Jamie from Desktop <jamiebclark@gmail.com>
2013-08-17 10:18:50 -0400

    Added validation to the Mappable Behavior

--- Model/Behavior/MappableBehavior.php
+++ Model/Behavior/MappableBehavior.php
@@ -8,6 +8,14 @@ class MappableBehavior extends ModelBehavior {
 			//'google' => false, TODO: Add Google Location Saving
 			'location' => true,
 			'timezone' => false,
+			
+			//Database fields
+			'addlineField' => 'addline',
+			'addlineCount' => 2,
+			'cityField' => 'city',
+			'stateField' => 'state',
+			'zipField' => 'zip',
+			'countryField' => 'country',
 		);
 		if (!isset($this->settings[$Model->alias])) {
 			$this->settings[$Model->alias] = $default;
@@ -15,17 +23,19 @@ class MappableBehavior extends ModelBehavior {
 		$this->settings[$Model->alias] = array_merge($this->settings[$Model->alias], (array) $settings);
 		
 		$settings =& $this->settings[$Model->alias];
+		extract($settings);
+		
 		//Binds to location models
 		$belongsTo = array();
 		if ($settings['location']) {
 			$belongsTo += array(
 				'State' => array(
 					'className' => 'Location.State',
-					'foreignKey' => 'state',
+					'foreignKey' => $stateField,
 				),
 				'Country' => array(
 					'className' => 'Location.Country',
-					'foreignKey' => 'country',
+					'foreignKey' => $countryField,
 				),
 			);
 		}
@@ -38,5 +48,33 @@ class MappableBehavior extends ModelBehavior {
 			ClassRegistry::init('Location.Timezone')->bindModel(array('hasMany' => array($Model->alias)), false);
 		}
 		$Model->bindModel(compact('belongsTo'), false);
+		
+		if ($validate) {
+			$fields = array(
+				$addlineField . '1' => 'Please enter an address',
+				$cityField => 'Please enter a city',
+				$stateField => 'Please enter a state',
+				$zipField => 'Please enter a zip code',
+				$countryField => 'Please select a country',
+			);
+			foreach ($fields as $field => $message) {
+				if (empty($field)) {
+					continue;
+				}
+				$this->mappableValidate($Model, $field, 'notEmpty', $message);
+			}
+		}
+	}
+	
+	function mappableValidate($Model, $field, $rule, $message = null) {
+		if (isset($Model->validate[$field])) {
+			if (!is_array($Model->validate[$field])) {
+				$Model->validate[$field] = array($Model->validate[$field]);
+			}
+		} else {
+			$Model->validate[$field] = array();
+		}
+		$Model->validate[$field][] = compact('rule', 'message');
+		return true;
 	}
 }
