1ed73d33e386ac3275097c42d612c428638bbced
Jamie from Desktop <jamiebclark@gmail.com>
2013-09-24 00:17:05 -0400

    Added Timezone selectList

--- Model/Timezone.php
+++ Model/Timezone.php
@@ -3,6 +3,14 @@ class Timezone extends LocationAppModel {
 	var $name = 'Timezone';	
 	var $recursive = -1;
 	
+	private $shortcuts = array(
+		-8 => 'Pacific Time',
+		-7 => 'Mountain Time',
+		-6 => 'Central Time',
+		-5 => 'Eastern Time',
+		-4 => 'Atlantic Time',
+	);
+
 	function findId($timezoneStr = null) {
 		//Finds user's timezone
 		if (empty($timezoneStr)) {
@@ -20,4 +28,41 @@ class Timezone extends LocationAppModel {
 			return false;
 		}
 	}
+	
+	function selectList() {
+		$result = $this->find('all');
+		$return = array();
+		foreach($result as $row) {
+			$row = $row[$this->alias];
+			$titles = explode('/', $row['title']);
+			if (count($titles) == 1) {
+				$title = $row['title'];
+				$category = '';
+			} else {
+				$category = array_shift($titles);
+				$title = implode('/', $titles);
+			}
+			if (!empty($row['abbr'])) {
+				$category = 'Shortcuts';
+				$title = $row['abbr'];
+			}
+			$return[$category][$row['id']] = $title;
+		}
+		return $return;
+	}
+	/*
+	function generate() {
+		$config = $this->getDataSource()->config;
+		$PDO = new PDO('mysql:host='.$config['host'].';dbname='.$config['database'].';charset=utf8;', $config['login'], $config['password']);
+		$timezones = $PDO->query('SELECT
+				TimeZoneName.Time_zone_id,
+				TimeZoneName.Name,
+				TimeZoneType.Offset
+			FROM mysql.time_zone_name AS TimeZoneName
+				JOIN mysql.time_zone_transition_type AS TimeZoneType ON TimeZoneType.Time_zone_id = TimeZoneName.Time_zone_id
+			WHERE TimeZoneType.Abbreviation = "GMT"
+			
+
+	}
+	*/
 }
